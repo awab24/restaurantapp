2d7c0fb1b76fa06e9cfbc55801143132d6370c64
Abdulbasid Ahmed <abdulbasidahmed937@gmail.com>
2025-12-17 14:16:18 +0200

    initial frontend setup

---
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className ='navbar'>
+    <img src={assets } alt="" />
+    </div>
+  )
+}
+
+export default Navbar
